ddc248b6a9436af361b542b7934829376b426c13
Philipp <zhanghongliang@192.168.1.101>
2020-08-15 12:03:49 +0800

    in static equilibrium

--- test/arithmatic.py
+++ test/arithmatic.py
@@ -3,12 +3,14 @@ import sys
 import unittest
 
 import numpy as np
+from numpy import array, cos, cross, radians, sin  # type: ignore
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 from arithmetic_analysis.bisection import bisection
 from arithmetic_analysis.gaussian_elimination import gaussian_elimination
+from arithmetic_analysis.in_static_equilibrium import in_static_equilibrium,polar_force
 
 
 class TDD_BISECTION(unittest.TestCase):
@@ -46,6 +48,14 @@ class TDD_BISECTION(unittest.TestCase):
         self.assertEqual(
             gaussian_elimination(mat[:2], [[5], [-7], [15]], returnList=True), []
         )
+    def test_in_static_equilibrium(self):
+        forces = array(
+        [polar_force(718.4, 180 - 30), polar_force(879.54, 45), polar_force(100, -90)]
+    )
+
+        location = array([[0, 0], [0, 0], [0, 0]])
+
+        self.assertTrue( in_static_equilibrium(forces, location))
 
 
 if __name__ == "__main__":
